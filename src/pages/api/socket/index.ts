22b573ffa36476e7181211388d3c9574f2987172
Ramon Xavier <ramon1234rxm@gmail.com>
2023-03-27 18:05:14 -0300

    fix: disable errors td

--- src/pages/api/socket/index.ts
+++ src/pages/api/socket/index.ts
@@ -1,13 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { Server } from "socket.io";
 
 const SocketHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  // @ts-ignore
   if (res.socket.server.io) {
     console.log("Socket is already running");
   } else {
     console.log("Socket is initializing");
+    // @ts-ignore
     const io = new Server(res.socket.server);
+
+    // @ts-ignore
     res.socket.server.io = io;
 
     io.on("connection", (socket) => {
