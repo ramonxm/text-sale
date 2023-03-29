c3358ea1cd6d9850876e42dc2ebeafa93b2199a7
Ramon Xavier <ramon1234rxm@gmail.com>
2023-03-29 10:42:23 -0300

    chore: testing redirects

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,33 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "~/styles/globals.css";
+import { useRouter } from "next/router";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [queryClient] = useState(() => new QueryClient());
+
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    const handleOffline = () => setIsOnline(false);
+    const handleOnline = () => setIsOnline(true);
+
+    window.addEventListener("offline", handleOffline);
+    window.addEventListener("online", handleOnline);
+
+    return () => {
+      window.removeEventListener("offline", handleOffline);
+      window.removeEventListener("online", handleOnline);
+    };
+  }, []);
+
+  const router = useRouter();
+
+  if (!isOnline) {
+    void router.push("/offline");
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
